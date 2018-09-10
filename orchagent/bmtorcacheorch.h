9979309d8b90a3cb2dad12fcd4c0ca238e89c6fd
yonatanp <yonatanp@mellanox.com>
2018-09-10 14:22:56 +0300

    orchagent/bmtorcacheorch.cpp

--- orchagent/bmtorcacheorch.h
+++ orchagent/bmtorcacheorch.h
@@ -42,29 +42,29 @@ private:
     sai_object_id_t sai_get_port_id_by_alias(std::string alias);
     sai_object_id_t sai_get_port_id_by_front_port(uint32_t hw_port);
     IpAddress gTunnelSrcIp;;
-    uint16_t gVlansStart;
-    uint32_t gVni;
-    uint16_t gDPDKVlan;
+    /* uint16_t gVlansStart; */
+    /* uint32_t gVni; */
+    /* uint16_t gDPDKVlan; */
     uint16_t gVnetBitmap;
     map<std::string, sai_object_id_t> vhost_entries;
     map<std::string, IpAddress> tunnel_ip_map;
-    map<std::string, sai_object_id_t> vnet_vlan_map;
+    /* map<std::string, sai_object_id_t> vnet_vlan_map; */
     bool getVhostEntry(std::string key, sai_object_id_t &entry_id);
     void setVhostEntry(std::string key, sai_object_id_t entry_id);
     bool removeVhostEntry(sai_object_id_t entry_id);
     bool getTunnelIP(std::string key, IpAddress &IP);
     void setTunnelIP(std::string key, IpAddress IP);
-    bool getVnetVlan(std::string key, sai_object_id_t &Vlan);
-    void setVnetVlan(std::string key, sai_object_id_t Vlan);
-    void create_dpdk_bridge_port();
-    sai_object_id_t create_vlan(uint16_t vid);
-    sai_object_id_t add_dpdk_vlan_member(sai_object_id_t vlan_oid);
-    uint16_t get_new_vlan();
-    bool is_vlan_used(uint16_t vid);
+    /* bool getVnetVlan(std::string key, sai_object_id_t &Vlan); */
+    /* void setVnetVlan(std::string key, sai_object_id_t Vlan); */
+    /* void create_dpdk_bridge_port(); */
+    /* sai_object_id_t create_vlan(uint16_t vid); */
+    /* sai_object_id_t add_dpdk_vlan_member(sai_object_id_t vlan_oid); */
+    /* uint16_t get_new_vlan(); */
+    /* bool is_vlan_used(uint16_t vid); */
     uint32_t GetFreeOffset();
     void RemoveOffset(sai_object_id_t entry_id);
     std::map<sai_object_id_t, uint32_t> used_offsets;
-    uint16_t get_vid_from_vlan(sai_object_id_t vlan_oid);
+    /* uint16_t get_vid_from_vlan(sai_object_id_t vlan_oid); */
     uint32_t gVhostTableSize;
     map<uint32_t, pair<sai_object_id_t, sai_object_id_t>> vniToBridgeMap;
     sai_object_id_t tunnel_encap_map;
